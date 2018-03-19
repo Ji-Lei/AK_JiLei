0b3e792682bdfd2dc5bec2888651bd8cd5a19e75
2701160109 <1597470950@qq.com>
2018-03-19 23:22:35 +0800

    By纪小雷

---
+++ AK_JiLei/src/MySelfWork/HolleWord.java
@@ -0,0 +1,14 @@
+package MySelfWork;
+
+/**
+ * Created by ${纪雷} on 2018/3/19.
+ */
+public class HolleWord {
+    public static void main(String[] args) {
+        String My="Holle Word!";
+        Mac(My);
+    }
+    private static void Mac(String s){
+        System.out.println(s);
+    }
+}
